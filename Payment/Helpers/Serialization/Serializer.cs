7fa56aca92c198c36fe7e953a707de0e06c1ecbc
shada <shadab33@hotmail.com>
2021-03-05 09:55:09 +0500

    added helpers and validations

---
+++ Payment/Helpers/Serialization/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Payment.Helpers.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment.Helpers.Serialization
+{
+    public class Serializer
+    {
+        public static string Serialize(object value, bool typeNameHandling = true)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings { DateFormatString = SystemSettings.JsonDateTimePattern, ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+
+            if (typeNameHandling)
+            {
+                settings.TypeNameHandling = TypeNameHandling.Objects;
+            }
+
+            string json = JsonConvert.SerializeObject(value, settings);
+
+            return json;
+        }
+    }
+}
---
+++ Payment/Helpers/Settings/SystemSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment.Helpers.Settings
+{
+    public static class SystemSettings
+    { 
+        public const string JsonDateTimePattern = "yyyy-MM-ddTHH:mm:ss.fffZ"; 
+    }
+}
